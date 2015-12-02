e84d16c3c4ad56c320d9be7892fdc2f8205ecc51
Kody Brown <thewizard@wasatchwizard.com>
2015-12-02 13:30:40 -0700

    Initial commit..

---
+++ ConsoleEx-Redirection.cs
@@ -0,0 +1,89 @@
+﻿/*!
+	Copyright (C) 2008-2015 Kody Brown (kody@bricksoft.com).
+	
+	MIT License:
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to
+	deal in the Software without restriction, including without limitation the
+	rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+	DEALINGS IN THE SOFTWARE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Bricksoft.PowerCode
+{
+	// Original source:
+	// http://stackoverflow.com/questions/3453220/how-to-detect-if-console-in-stdin-has-been-redirected/3453272#3453272
+	public partial class ConsoleEx
+	{
+		private static List<DeleteFileWhenDone> _deleteFiles = new List<DeleteFileWhenDone>();
+
+		public static bool IsOutputRedirected
+		{
+			get { return FileType.Char != GetFileType(GetStdHandle(StdHandle.Stdout)); }
+		}
+
+		public static bool IsInputRedirected
+		{
+			get { return FileType.Char != GetFileType(GetStdHandle(StdHandle.Stdin)); }
+		}
+
+		public static bool IsErrorRedirected
+		{
+			get { return FileType.Char != GetFileType(GetStdHandle(StdHandle.Stderr)); }
+		}
+
+		public static string RedirectInputToFile()
+		{
+			string f = Path.GetTempFileName();
+			File.WriteAllText(f, Console.In.ReadToEnd());
+			_deleteFiles.Add(new DeleteFileWhenDone(f));
+			return f;
+		}
+
+		// P/Invoke:
+		private enum FileType { Unknown, Disk, Char, Pipe };
+		private enum StdHandle { Stdin = -10, Stdout = -11, Stderr = -12 };
+
+		[DllImport("kernel32.dll")]
+		private static extern FileType GetFileType( IntPtr hdl );
+
+		[DllImport("kernel32.dll")]
+		private static extern IntPtr GetStdHandle( StdHandle std );
+
+		private class DeleteFileWhenDone
+		{
+			public string _file { get; private set; }
+
+			public DeleteFileWhenDone( string file )
+			{
+				_file = file;
+			}
+
+			~DeleteFileWhenDone()
+			{
+				if (File.Exists(_file)) {
+					File.SetAttributes(_file, FileAttributes.Normal);
+					File.Delete(_file);
+				}
+			}
+		}
+	}
+}
---
+++ ShowOutput.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Bricksoft.PowerCode;
+
+namespace ViewStandardIn
+{
+	class ShowOutput
+	{
+		static int Main( string[] args )
+		{
+			bool optWait = false;
+			string tmpfile = "";
+			string input = "";
+			Process p;
+			ProcessStartInfo startInfo;
+
+			for (int i = 0; i < args.Length; i++) {
+				string a = args[i].Trim();
+
+				if (a.Equals("version", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("--version", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("-version", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("/version", StringComparison.CurrentCultureIgnoreCase)) {
+					DisplayVersion(true);
+					return 0;
+				} else if (a.Equals("--v", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("-v", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("/v", StringComparison.CurrentCultureIgnoreCase)) {
+					DisplayVersion(false);
+					return 0;
+
+				} else if (a.Equals("wait", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("--wait", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("-wait", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("/wait", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("-w", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("/w", StringComparison.CurrentCultureIgnoreCase)) {
+					optWait = true;
+
+				} else if (a.Equals("file", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("--file", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("-file", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("/file", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("-f", StringComparison.CurrentCultureIgnoreCase)
+						|| a.Equals("/f", StringComparison.CurrentCultureIgnoreCase)) {
+					if (i == args.Length - 1) {
+						DisplayVersion(true);
+						Console.Out.WriteLine();
+						Console.Error.WriteLine("    **** ERROR: --file is missing file name", a);
+						return 4;
+					}
+					tmpfile = args[i + 1];
+					input = File.ReadAllText(tmpfile);
+
+				} else {
+					DisplayVersion(true);
+					Console.Out.WriteLine();
+					Console.Error.WriteLine("    **** ERROR: unknown command: {0}", a);
+					return 3;
+				}
+			}
+
+			if (!ConsoleEx.IsInputRedirected && (args.Length == 0 || tmpfile.Length == 0)) {
+				DisplayVersion(true);
+				Console.Out.WriteLine();
+				Console.Error.WriteLine("    **** ERROR: missing content to display");
+				return 1;
+			}
+
+			// Read stdin
+			if (ConsoleEx.IsInputRedirected && tmpfile.Length == 0) {
+				input = Console.In.ReadToEnd();
+				tmpfile = Path.GetTempFileName() + ".txt";
+				File.WriteAllText(tmpfile, input);
+			}
+
+			// Show the file in the default .txt editor.
+			startInfo = new ProcessStartInfo();
+			startInfo.Arguments = "";
+			startInfo.FileName = tmpfile;
+			startInfo.Verb = "Open";
+			startInfo.WorkingDirectory = Environment.CurrentDirectory;
+
+			p = Process.Start(startInfo);
+
+			if (optWait) {
+				p.WaitForExit(30000);
+			}
+
+			return 0;
+		}
+
+		private static void DisplayVersion( bool full )
+		{
+			FileVersionInfo fi = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location);
+			if (full) {
+				Console.Out.WriteLine("{4} v{0}.{1}.{2}.{3}", fi.FileMajorPart, fi.FileMinorPart, fi.ProductBuildPart, fi.ProductPrivatePart, fi.OriginalFilename);
+				Console.Out.WriteLine(fi.LegalCopyright);
+				Console.Out.WriteLine("Saves its stdin to a temporary file and opens it in the default text editor.");
+			} else {
+				Console.Out.WriteLine("{0}.{1}.{2}.{3}", fi.FileMajorPart, fi.FileMinorPart, fi.ProductBuildPart, fi.ProductPrivatePart);
+			}
+		}
+	}
+}
